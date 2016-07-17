57c6ecf2b11a8b2f8ab6085361f72f0301e2bf0c
Jon Zobrist <jon@jonzobrist.com>
2016-07-17 15:12:00 -0700

    sorting hello

--- hello.py
+++ hello.py
@@ -6,8 +6,11 @@
 # peoples = ['Claire', 'Alex', 'Blake', 'Jon','Lemon']
 # Dictionary of their favorite items
 favorite_items = {'Claire':'Pokemon','Alex':'Magic Cards',
-                  'Blake':'His spot on the couch'}
+                  'Blake':'His spot on the couch',
+                  'Mom':'Napping',
+                  'Dad':'Drinking wine (because he\'s an alki)'}
+
 my_string = "{} really loves {}!"
 
-for person in favorite_items:
+for person in sorted(favorite_items):
     print(my_string.format(person, favorite_items[person]))
