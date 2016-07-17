102f0a6e40beaf83cd37a3e0a56d42ec37f06f69
Jon Zobrist <jon@jonzobrist.com>
2016-07-17 14:21:20 -0700

    adding list with bug

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 #!python
 # This is my first python program, it says hello world!
 
+peoples = ['Claire', 'Alex', 'Blake', 'Jon','Lemon']
+
 for number in xrange(0,10):
-    print "My number is {}".format(number)
+    print "My person is {}".format(peoples[number])
