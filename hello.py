d8aca7842b19c1fc3febad6ab85d96640f2740ec
Jon Zobrist <jon@jonzobrist.com>
2016-07-17 14:36:25 -0700

    bro

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 #!python
 # This is my first python program, it says hello world!
 
+# List of people
 peoples = ['Claire', 'Alex', 'Blake', 'Jon','Lemon']
+# Dictionary of their favorite items
+favorite_items = {'Claire':'Pokemon','Alex':'Magic Cards',
+                  'Blake':'Osama Bin Laden'}
 
-for number in xrange(0,len(peoples)):
-    print "My person is {}".format(peoples[number])
+for number in xrange(len(peoples)):
+    print "{}'s favorite item is {}'".format(peoples[number],
+                                      favorite_items[peoples[number]])
