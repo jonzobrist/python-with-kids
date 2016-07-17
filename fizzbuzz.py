54ad00386aea1a713a356412565484af224f27cb
Jon Zobrist <jon@jonzobrist.com>
2016-07-17 14:45:28 -0700

    initial fizzbuzz with comments

---
+++ fizzbuzz.py
@@ -0,0 +1,8 @@
+#!python
+
+# Fizz Buzz:
+# Iterate over numbers printing Fizz if the numbers
+# is divisble by 3 you say Fizz
+# if it's divisible by 5 you say Buzz
+# both? FizzBuzz
+# neither? Just say the number, punk!
