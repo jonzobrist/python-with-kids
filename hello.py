2bb3c4448c3ee7eebd65fd431882096bd717f260
Jon Zobrist <jon@jonzobrist.com>
2016-07-17 14:42:49 -0700

    change to use dict only no list

--- hello.py
+++ hello.py
@@ -2,11 +2,12 @@
 # This is my first python program, it says hello world!
 
 # List of people
-peoples = ['Claire', 'Alex', 'Blake', 'Jon','Lemon']
+# no longer needed!
+# peoples = ['Claire', 'Alex', 'Blake', 'Jon','Lemon']
 # Dictionary of their favorite items
 favorite_items = {'Claire':'Pokemon','Alex':'Magic Cards',
-                  'Blake':'Osama Bin Laden'}
+                  'Blake':'His spot on the couch'}
+my_string = "{} really loves {}!"
 
-for number in xrange(len(peoples)):
-    print "{}'s favorite item is {}'".format(peoples[number],
-                                      favorite_items[peoples[number]])
+for person in favorite_items:
+    print(my_string.format(person, favorite_items[person]))
