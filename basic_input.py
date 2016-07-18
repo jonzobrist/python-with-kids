ca068bd083457f1edbb610923e0e453d20905bf7
Jon Zobrist <jon@jonzobrist.com>
2016-07-17 17:25:50 -0700

    adding python stackskills stuff

--- basic_input.py
+++ basic_input.py
@@ -2,13 +2,8 @@
 """ playing with some basic input and output """
 import sys
 
-# Unique ones including the script name
-# arguments = set(sys.argv)
-# print "Passed in {} arguments, with {} unique ones, they are :".format(len(sys.argv), len(arguments))
-
-# Unique arguments not including the script name
-arguments = set()
-print "Passed in {} arguments, they are :".format(len(sys.argv) - 1)
+my_peoples = set()
+print "Passed in {} people, they are :".format(len(sys.argv) - 1)
 for arg in xrange(1, len(sys.argv)):
      arguments.add(sys.argv[arg])
 
